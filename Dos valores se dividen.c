fc395873d6a3af584d40feb2de33f9185e330522
Jose Jaime <jimmy17145@hotmail.com>
2021-10-13 21:58:47 -0500

    numero con switch

---
+++ Dos valores se dividen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+     int  cant1, cant2, i, j;
+
+    /*printf("Escribe la 1era. cantidad: ");
+    scanf("%d", &cant1);
+
+    printf("Escribe la 2da. cantidad: ");
+    scanf("%d", &cant2);
+
+    if(cant1 >= cant2)
+        if(cant1 % cant2 == 0)
+            printf("%d Divide a %d", cant2, cant1);
+        else
+            printf("%d y %d no se dividen", cant2, cant1);
+    else
+        if(cant1 < cant2)
+            if(cant2 % cant1 == 0)
+                printf("%d Divide a %d", cant1, cant2);
+            else
+                printf("%d y %d no se dividen", cant1, cant2);*/
+
+}
+
+
+
